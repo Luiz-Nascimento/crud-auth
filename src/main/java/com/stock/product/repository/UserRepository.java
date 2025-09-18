0ce4cbf84d92ba3dd9ce33eeb409e5e328fe8767
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-09-18 17:01:33 -0300

    Created UserRepository

---
+++ src/main/java/com/stock/product/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.stock.product.repository;
+
+import com.stock.product.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    UserDetails findByUsername(String username);
+}
